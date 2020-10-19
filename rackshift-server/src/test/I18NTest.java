05be7477cd25a5356b4ab0a5deaec87559228535
rackshift <330842973@qq.com>
2020-10-19 19:35:24 +0800

    fix:参数校验国际化

--- rackshift-server/src/test/I18NTest.java
+++ rackshift-server/src/test/I18NTest.java
@@ -10,9 +10,10 @@ import java.util.regex.Pattern;
 
 public class I18NTest {
 
+    //提取项目里面国际化的关键字
     public static void main(String[] args) throws IOException {
-        File file = new File("D:\\rackshift\\rackshift-web\\src\\App.vue");
-//        File file = new File("D:\\rackshift\\rackshift-web\\src\\rackparams");
+        File file = new File("D:\\rackshift\\rackshift-web\\src\\common\\validator\\CommonValidator.js");
+//        File file = new File("D:\\rackshift\\rackshift-web\\src\\components");
         File file2 = new File("D:\\rackshift\\rackshift-web\\src\\i18n\\zh-CN.json");
         BufferedReader reader = new BufferedReader(new FileReader(file2));
         String line = null;
@@ -32,7 +33,7 @@ public class I18NTest {
                     extract(listFile, i18n);
                 }
             }
-        }else{
+        } else {
             extract(file, i18n);
         }
 
@@ -53,7 +54,7 @@ public class I18NTest {
                 sb.append(line + "\n");
             }
 
-            Pattern p = Pattern.compile("\\$t\\('([a-zA-Z]+)'\\)");
+            Pattern p = Pattern.compile("\\$t\\('([a-zA-Z]+_*[a-zA-Z]+)'\\)");
 //                            Pattern p = Pattern.compile("^\\w+'(i18n_[\\w]+)' | translator :'([\\u4e00-\\u9fa5]+)'");
             Matcher m = p.matcher(sb.toString());
             while (m.find()) {
@@ -63,4 +64,11 @@ public class I18NTest {
             }
         }
     }
+
+
+//    public static void main(String[] args) {
+//        Pattern p = Pattern.compile("\\$t\\('([a-zA-Z]+_*[a-zA-Z]+)'\\)");
+//        Matcher m = p.matcher("$t('discoveryed')");
+//        System.out.println(m.find());
+//    }
 }
