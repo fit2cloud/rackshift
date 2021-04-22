5be069e06b5bb2d1de151e9d79eb9718203e41c8
zhangdahai112 <dahai.zhang@fit2cloud.com>
2021-04-22 13:36:11 +0800

    fix:插件镜像问题

---
+++ rackshift-server/src/main/resources/db/migration/V20__fixplugin.sql
@@ -0,0 +1,6 @@
+﻿update plugin
+set image = 'registry.cn-qingdao.aliyuncs.com/x-lab/rackshift/kciepluc/racadm-docker'
+where image = 'kciepluc/racadm-docker';
+update plugin
+set image = 'registry.cn-qingdao.aliyuncs.com/x-lab/rackshift/kfox1111/ipmitool:latest'
+where image = 'kfox1111/ipmitool';
