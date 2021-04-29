7b634c0011562b7465203e44a01d38dc6b8a8015
zhangdahai112 <dahai.zhang@fit2cloud.com>
2021-04-29 12:03:27 +0800

    docs:修改 registry

---
+++ rackshift-server/src/main/resources/db/migration/V22__pluginregistry.sql
@@ -0,0 +1,6 @@
+﻿update plugin
+set image = 'registry.cn-qingdao.aliyuncs.com/x-lab/racadm-docker'
+where image = 'registry.cn-qingdao.aliyuncs.com/x-lab/rackshift/kciepluc/racadm-docker';
+update plugin
+set image = 'registry.cn-qingdao.aliyuncs.com/x-lab/ipmitool:latest'
+where image = 'registry.cn-qingdao.aliyuncs.com/x-lab/rackshift/kfox1111/ipmitool:latest';
