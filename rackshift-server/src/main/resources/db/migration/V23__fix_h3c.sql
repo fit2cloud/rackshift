e8ec586af072d7e40fb5a04573f5f17ccfa6314c
zhangdahai112 <dahai.zhang@fit2cloud.com>
2021-05-18 12:02:23 +0800

    fix:flyway 报错

--- rackshift-server/src/main/resources/db/migration/V23__fix_h3c.sql
+++ rackshift-server/src/main/resources/db/migration/V23__fix_h3c.sql
@@ -1,8 +1,4 @@
-﻿CREATE TABLE wf_bak_2021_05_14 AS
-SELECT *
-FROM workflow;
-
-ALTER TABLE bare_metal MODIFY COLUMN machine_model VARCHAR (100) DEFAULT '' COMMENT '机器型号';
+﻿ALTER TABLE bare_metal MODIFY COLUMN machine_model VARCHAR (100) DEFAULT '' COMMENT '机器型号';
 
 ALTER TABLE workflow MODIFY COLUMN brands VARCHAR (1000) DEFAULT '' COMMENT '支持的品牌';
 
