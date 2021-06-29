05ec6ca178887e7bc19b9b4f81642e1046d880de
zhangdahai112 <dahai.zhang@fit2cloud.com>
2021-06-29 15:06:35 +0800

    feat: 增加 windowsServer2019 支持

---
+++ rackshift-server/src/main/resources/db/migration/V29__windowsServer2019.sql
@@ -0,0 +1,35 @@
+﻿
+insert into workflow
+values (20,
+        'system',
+        'Graph.InstallWindowsServer2016',
+        '安装 windowsServer 2019 标准版',
+        'POST_OS_WORKFLOW_START',
+        '[\'DELL\', \'HP\', \'Inspur\',\'ZTE\',\'Huawei\', \'New H3C Technologies Co., Ltd.\']',
+        'true',
+        '{
+  "options": {
+    "defaults": {
+      "hostname": "localhost",
+      "domain": "rackshift",
+      "password": "RackShift",
+      "username": "onrack",
+      "firewallDisable": true,
+      "networkDevices": [
+        {
+          "device": null,
+          "ipv4": {
+            "ipAddr": "172.31.128.152",
+            "gateway": "172.31.128.5",
+            "netmask": "255.255.255.0"
+          }
+        }
+      ],
+      "productkey": null,
+      "smbUser": "www",
+      "smbPassword": "www",
+      "smbRepo": null,
+      "repo": null
+    }
+  }
+}', 'enable', now());
