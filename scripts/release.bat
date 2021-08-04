69bd44513c682c1c7bb93b973eb086121a69309b
zhangdahai112 <dahai.zhang@fit2cloud.com>
2021-08-04 21:46:42 +0800

    docs:更新离线包链接

--- scripts/release.bat
+++ scripts/release.bat
@@ -1,5 +1,5 @@
-git tag -d v1.1.0
-git push origin :refs/tags/v1.1.0
+git tag -d v1.4.0
+git push origin :refs/tags/v1.4.0
 
-git tag v1.1.0
-git push origin v1.1.0
+git tag v1.4.0
+git push origin v1.4.0
