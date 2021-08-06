fc9e4b949db555dcaddb29c12aa718b989a18f12
zhangdahai112 <dahai.zhang@fit2cloud.com>
2021-08-06 15:16:19 +0800

    feat: hpe gen10 格式兼容

---
+++ rackshift-server/src/main/resources/db/migration/V32__addhpe.sql
@@ -0,0 +1,17 @@
+UPDATE workflow
+SET brands = '[\'HP\',\'HPE\']'
+WHERE
+    id IN (8, 9, 10);
+
+UPDATE workflow
+SET brands = '[\'DELL\', \'HP\', \'Inspur\',\'ZTE\', \'Huawei\', \'New H3C Technologies Co., Ltd.\', \'Lenovo\', \'HPE\',]'
+WHERE
+    injectable_name IN (
+    'Graph.InstallCentOS',
+    'Graph.InstallESXi',
+    'Graph.InstallRHEL',
+    'Graph.InstallUbuntuLiveCD',
+    'Graph.InstallWindowsServer',
+    'Graph.InstallWindowsServer2016',
+    'Graph.InstallWindowsServer2019'
+    );
