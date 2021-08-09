2a638aa2b8b5f0fbeb5f5f1358ee07426a94bb84
zhangdahai112 <dahai.zhang@fit2cloud.com>
2021-08-09 14:09:56 +0800

    build:1.5.0

--- rackshift-web/src/common/about/AboutUs.vue
+++ rackshift-web/src/common/about/AboutUs.vue
@@ -47,7 +47,7 @@ export default {
       dialogVisible: false,
       githubUrl: 'https://github.com/rackshift/rackshift',
       websiteUrl: 'https://rackshift.io',
-      version: 'Release v1.4.0'
+      version: 'Release v1.5.0'
     }
   },
   created() {
