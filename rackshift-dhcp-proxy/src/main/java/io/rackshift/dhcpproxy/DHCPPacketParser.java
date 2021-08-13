ba9b6b9dd5612cbfff5a281c6b06bfb4043866c0
zhangdahai112 <dahai.zhang@fit2cloud.com>
2021-08-13 16:33:28 +0800

    fix:补充 options 解析

--- rackshift-dhcp-proxy/src/main/java/io/rackshift/dhcpproxy/DHCPPacketParser.java
+++ rackshift-dhcp-proxy/src/main/java/io/rackshift/dhcpproxy/DHCPPacketParser.java
@@ -14,6 +14,7 @@ public class DHCPPacketParser {
         JSONObject packet = new JSONObject();
         int code = 0;
         int offset = 240;
+        int len = 0;
         JSONArray unhandledOptions = new JSONArray();
         //rfc2132 解析 dhcp packet
         packet.put("op", DHCPProtocolConstants.MessageType.getByCode(Integer.valueOf(dataByte[0])));
@@ -27,15 +28,17 @@ public class DHCPPacketParser {
         packet.put("siaddr", ByteUtil.readIp(dataByte, 20));
         packet.put("giaddr", ByteUtil.readIp(dataByte, 24));
         packet.put("chaddr", ByteUtil.readMAC(dataByte, 28, (Integer) packet.get("hlen")));
-        packet.put("sname", byteBuf.toString(44, 64, Charset.forName("ascii")));
-        packet.put("fname", byteBuf.toString(108, 128, Charset.forName("ascii")));
+        packet.put("sname", byteBuf.toString(44, 64, Charset.forName("ascii")).trim());
+        packet.put("fname", byteBuf.toString(108, 128, Charset.forName("ascii")).trim());
 
         //rfc2132 解析 dhcp packet options
         JSONObject options = new JSONObject();
+
         while (code != 255 && dataByte.length > offset) {
             code = ByteUtil.readUInt8(dataByte, offset);
             offset++;
-            int len = 0;
+            len = ByteUtil.readUInt8(dataByte, offset);
+            offset++;
             switch (code) {
                 case 0:
                     continue;
@@ -51,25 +54,115 @@ public class DHCPPacketParser {
                     break;
                 case 3:
                     JSONArray routerOptions = new JSONArray();
-                    int length = ByteUtil.readUInt8(dataByte, offset);
-                    offset++;
-                    for (int i = 0; i < length; i++) {
+                    for (int i = 0; i < len; i++) {
                         String ip = ByteUtil.readIp(dataByte, offset);
                         routerOptions.add(ip);
                         offset += 4;
                     }
                     options.put("routerOptions", routerOptions);
                     break;
+                case 4:
+                    JSONArray timeServerOption = new JSONArray();
+                    for (int i = 0; i < len; i++) {
+                        int time = ByteUtil.readUInt32(dataByte, offset);
+                        timeServerOption.add(time);
+                        offset += 4;
+                    }
+                    options.put("timeServerOption", timeServerOption);
+                    break;
+
+                case 5:
+                    JSONArray nameServerOption = new JSONArray();
+                    for (int i = 0; i < len; i++) {
+                        int name = ByteUtil.readUInt32(dataByte, offset);
+                        nameServerOption.add(name);
+                        offset += 4;
+                    }
+                    options.put("nameServerOption", nameServerOption);
+                    break;
+
+                case 6:
+                    JSONArray domainServerOption = new JSONArray();
+                    for (int i = 0; i < len; i++) {
+                        int name = ByteUtil.readUInt32(dataByte, offset);
+                        domainServerOption.add(name);
+                        offset += 4;
+                    }
+                    options.put("domainServerOption", domainServerOption);
+                    break;
+
+                case 12:
+                    options.put("hostName", byteBuf.toString(offset, offset + len, Charset.forName("ascii")).trim());
+                    offset += len;
+                    break;
+
+                case 15:
+                    options.put("domainName", byteBuf.toString(offset, offset + len, Charset.forName("ascii")).trim());
+                    offset += len;
+                    break;
+
+                case 28:
+                    options.put("broadcastAddress", byteBuf.toString(offset, offset + len, Charset.forName("ascii")).trim());
+                    offset += len;
+                    break;
+
+                case 43:
+                    JSONArray vendorOptions = new JSONArray();
+                    for (int i = 0; i < len; i++) {
+                        int vendorOpt = ByteUtil.readUInt8(dataByte, offset);
+                        offset++;
+                        int vendorLen = ByteUtil.readUInt8(dataByte, offset);
+                        String optionContent = byteBuf.toString(offset, vendorLen, Charset.forName("ascii"));
+                        JSONObject obj = new JSONObject();
+                        obj.put("vendorOpt", vendorOpt);
+                        obj.put("optionContent", optionContent);
+                        vendorOptions.add(obj);
+                        offset += vendorLen;
+                    }
+                    options.put("vendorOptions", vendorOptions);
+                    break;
+
+                case 50:
+                    options.put("requestedIpAddress", byteBuf.toString(offset, offset + 4, Charset.forName("ascii")));
+                    offset += len;
+                    break;
+
+                case 51:
+                    options.put("ipAddressLeaseTime", ByteUtil.readUInt32(dataByte, offset));
+                    offset += len;
+                    break;
+
+                case 52:
+                    options.put("optionOverload", ByteUtil.readUInt8(dataByte, offset));
+                    offset += len;
+                    break;
+
+                case 53:
+                    options.put("dhcpMessageType", DHCPProtocolConstants.DHCPMessageType.getByCode(ByteUtil.readUInt8(dataByte, offset)));
+                    offset += len;
+                    break;
+
+                case 54:
+                    options.put("serverIdentifier", ByteUtil.readIp(dataByte, offset));
+                    offset += len;
+                    break;
+
+                case 60:
+                    options.put("vendorClassIdentifier", byteBuf.toString(offset, offset + len, Charset.forName("ascii")));
+                    offset += len;
+                    break;
+
                 default: {
-                    len = ByteUtil.readUInt8(dataByte, offset);
-                    offset += 1;
                     offset += len;
                     break;
                 }
             }
-
-            packet.put("options", options);
-
-            return packet;
         }
+
+        packet.put("options", options);
+
+        return packet;
+
+
     }
+}
--- rackshift-dhcp-proxy/src/main/java/io/rackshift/dhcpproxy/DHCPProtocolConstants.java
+++ rackshift-dhcp-proxy/src/main/java/io/rackshift/dhcpproxy/DHCPProtocolConstants.java
@@ -16,6 +16,29 @@ public class DHCPProtocolConstants {
                 }
             }
 
+            throw new RuntimeException("Invalid Message Type value: " + code);
+        }
+
+    }
+
+    public enum DHCPMessageType {
+        DHCPDISCOVER(1), DHCPOFFER(2),
+        DHCPREQUEST(3), DHCPDECLINE(4),
+        DHCPACK(5), DHCPNAK(6),
+        DHCPRELEASE(7), DHCPINFORM(8);
+        int code;
+
+        DHCPMessageType(int code) {
+            this.code = code;
+        }
+
+        static DHCPMessageType getByCode(int code) {
+            for (DHCPMessageType t : values()) {
+                if (t.code == code) {
+                    return t;
+                }
+            }
+
             throw new RuntimeException("Invalid DHCP Message Type value: " + code);
         }
 
