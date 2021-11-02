8c2179080e2a700150c9dae55a4d9acd56657154
zhangdahai112 <dahai.zhang@fit2cloud.com>
2021-11-02 18:27:46 +0800

    fix:浪潮机器型号读取错误

--- rackshift-server/src/main/java/io/rackshift/engine/util/CatalogParser.java
+++ rackshift-server/src/main/java/io/rackshift/engine/util/CatalogParser.java
@@ -144,6 +144,7 @@ public class CatalogParser {
                     JSONObject fruDevice = fru.getJSONObject("Builtin FRU Device (ID 0)");
                     if (StringUtils.isAnyBlank(en.getModel(), en.getName(), en.getBrand())) {
                         en.setBrand(fruDevice.getString("Product Manufacturer"));
+                        en.setModel(fruDevice.getString("Product Name"));
                         if (StringUtils.isBlank(en.getModel())) {
                             en.setModel(fruDevice.getString("Board Product"));
                             en.setName(en.getBrand() + " " + en.getModel());
