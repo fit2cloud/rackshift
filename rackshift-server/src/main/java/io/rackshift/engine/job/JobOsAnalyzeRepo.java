53f8365814d7f22dbcb116e7e8a21559402d056d
zhangdahai112 <dahai.zhang@fit2cloud.com>
2021-11-29 22:36:01 -0800

    fix: validate esxi repo params

--- rackshift-server/src/main/java/io/rackshift/engine/job/JobOsAnalyzeRepo.java
+++ rackshift-server/src/main/java/io/rackshift/engine/job/JobOsAnalyzeRepo.java
@@ -1,7 +1,10 @@
 package io.rackshift.engine.job;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import io.rackshift.model.RSException;
 import io.rackshift.mybatis.mapper.TaskMapper;
+import io.rackshift.utils.HttpClientUtil;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.ApplicationContext;
 
@@ -38,9 +41,39 @@ public class JobOsAnalyzeRepo extends BaseJob {
     @Override
     public void run() {
         //validate esxi repo
-        JSONObject param = new JSONObject();
-        param.put("mbootFile", "mboot.c32");
-        updateAllOptions(param);
+        //download repo/boot.cfg to get cfg args
+        String content = "";
+        try {
+            content = HttpClientUtil.get(options.getString("repo") + "/boot.cfg", null);
+            updateAllOptions(getParam(content));
+        } catch (Exception e) {
+            this.error(RSException.throwExceptions("get esxi repo boot.cfg failed ! either the repo is not valid nor the file formate is not correct!"));
+            return;
+        }
         this.complete();
+    }
+
+    private JSONObject getParam(String content) {
+        JSONArray params = JSONArray.parseArray("[ {\n" +
+                "            key: 'tbootFile',\n" +
+                "                    pattern: 'kernel='\n" +
+                "        }, {\n" +
+                "            key: 'moduleFiles',\n" +
+                "                    pattern: 'modules='\n" +
+                "        }\n" +
+                "        ]");
+        JSONObject result = new JSONObject();
+        params.forEach(p -> {
+            result.put(((JSONObject) p).getString("key"), getValue(((JSONObject) p).getString("pattern"), content));
+        });
+
+        result.put("mbootFile", "mboot.c32");
+        return result;
+    }
+
+    private static String getValue(String pattern, String c) {
+        int startIndex = c.indexOf(pattern);
+        int endIndex = c.substring(startIndex + pattern.length()).indexOf("\n");
+        return c.substring(startIndex + pattern.length()).substring(0, endIndex);
     }
 }
