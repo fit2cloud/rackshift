dceaa4741b22232a5cead8ef06404501197f5539
zhangdahai112 <dahai.zhang@fit2cloud.com>
2021-12-28 14:26:12 +0800

    refactor: 日志打印优化

--- rackshift-server/src/main/java/io/rackshift/Application.java
+++ rackshift-server/src/main/java/io/rackshift/Application.java
@@ -2,7 +2,9 @@ package io.rackshift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -10,8 +12,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @PropertySource(value = {"file:/opt/rackshift/conf/rackshift.properties"}, encoding = "UTF-8", ignoreResourceNotFound = true)
 @SpringBootApplication
 @EnableScheduling
-public class Application {
+public class Application implements ApplicationListener<ApplicationReadyEvent> {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+        System.out.println("======================服务启动完毕=========================");
     }
 }
